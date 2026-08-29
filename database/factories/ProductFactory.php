5d05535596cc06383293c1f9643bfda2fc6a492b
Leo <bmartinezd3@miumg.edu.gt>
2026-08-29 04:18:30 +0000

    factory: crear ProductFactory para tests

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->words(2, true),
+            'description' => fake()->sentence(),
+            'price' => fake()->randomFloat(2, 10, 500),
+            'stock' => fake()->numberBetween(0, 100),
+        ];
+    }
+}
